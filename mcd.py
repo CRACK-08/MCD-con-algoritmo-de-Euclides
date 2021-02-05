79d073031f52196fc0ce07bee19c16567a816a7d
CRACK-08 <61031736+CRACK-08@users.noreply.github.com>
2021-02-05 17:53:51 -0600

    Orly.py

--- mcd.py
+++ mcd.py
@@ -1,30 +1,21 @@
-class maximoDivisor:
-    c = ""
-    def __init__(self):
-        print("creado")
+# INSTRUCCIONES:
+# Corregir la implementación del algoritmo de Euclides
 
-    def prueba(self,a,b):
-        if b > a:
-            return self.prueba(b,a)
-        if b == 0:
-            return a,self.c
-        else:
-            q,r = divmod(a,b)
-            print(a,"=",q,"*",b,"+",r)
-            self.c += str(a) + " = " + str(q) + " * " + str(b) + " + " + str(r) + "\n"
-            return self.prueba(b,r)
+# 1.- Damos valores iniciales
+print ( "Valores iniciales" )
+A  =  270
+B  =  192
+imprimir ( A )
+imprimir ( B )
+# 2.- Ciclo del algoritmo
+para  i  en el  rango ( 5 ):
+  print ( "Iteración"  +  str ( i ))
+  R  =  A  %  B
+  imprimir ( "R ="  +  str ( R ))
 
-    def extended_euclid_gcd(self,a, b):
-        s = 0; old_s = 1
-        t = 1; old_t = 0
-        r = b; old_r = a
-
-        while r != 0:
-            quotient = old_r//r 
-            old_r, r = r, old_r - quotient*r
-            old_s, s = s, old_s - quotient*s
-            old_t, t = t, old_t - quotient*t
-        return [old_r, old_s, old_t]
-
-
-
+  A  =  B
+  B  =  R
+  imprimir ( "A ="  +  str ( A ))
+  imprimir ( "B ="  +  str ( B ))
+# Resultados (asignar el resultado del MCD a la variable M)
+print ( "El MCD es ="  +  str ( M ))
